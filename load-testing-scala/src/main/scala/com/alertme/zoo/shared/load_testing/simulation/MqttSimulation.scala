12804691c29cb8d51b92055ffb5761bae927f726
Alessio Santacroce <Alessio.Santacroce@bgch.co.uk>
2017-01-11 10:06:49 +0000

    xxx

--- load-testing-scala/src/main/scala/com/alertme/zoo/shared/load_testing/simulation/MqttSimulation.scala
+++ load-testing-scala/src/main/scala/com/alertme/zoo/shared/load_testing/simulation/MqttSimulation.scala
@@ -1,13 +1,15 @@
 package com.alertme.zoo.shared.load_testing.simulation
 
+import com.alertme.zoo.shared.awsloadtesting.scrty.SecurityTools
 import com.github.mnogu.gatling.mqtt.Predef._
 import io.gatling.core.Predef._
 import org.fusesource.mqtt.client.QoS
-import com.alertme.zoo.shared.awsloadtesting.scrty.SecurityTools
 
 import scala.concurrent.duration._
 
 class MqttSimulation extends Simulation {
+
+  // arn:aws:iot:eu-west-1:886358885274:thing/alessio-test
 
   before {
     println("***** My simulation is about to begin! *****")
@@ -19,17 +21,25 @@ class MqttSimulation extends Simulation {
   }
 
   val mqttConf = mqtt
-    .host("ssl://ay308tk1woz4x.iot.eu-west-1.amazonaws.com:8883")
-    .clientId("vtkachevSDKThing1")
+    .host("tls://a3gemn1nf8j60e.iot.eu-west-1.amazonaws.com:8883")
+    .clientId("alessio-test")
 
-  val pld = "{}"
-
+  val pld =
+    """{
+      |    "state": {
+      |        "desired" : {
+      |            "color" : { "b" : 10 },
+      |            "engine" : "OFF"
+      |        }
+      |    }
+      |}"""
   val scn = scenario("MQTT Test")
     .exec(mqtt("request")
-    .publish("$aws/things/vtkachevSDKThing1/shadow/update", pld, QoS.AT_LEAST_ONCE, retain = false))
+      .publish("$aws/things/alessio-test/shadow/update", pld, QoS.AT_LEAST_ONCE, retain = false))
+    .exitHereIfFailed
 
   setUp(
     scn
-      .inject(constantUsersPerSec(10) during(90 seconds)))
+      .inject(constantUsersPerSec(10) during (90 seconds)))
     .protocols(mqttConf)
 }
